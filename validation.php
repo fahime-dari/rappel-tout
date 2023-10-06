1da8a7c42ec0af75190ac02b2ed7408f0d611d34
fahime-dari <128680020+fahime-dari@users.noreply.github.com>
2023-10-06 08:13:37 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -7,12 +7,15 @@
     </head>
     <body>
         <center>
+            // Formulaire de confirmation de saisie 
             <form action="confirmation.php" method="POST" >
                 <h1>AJOUT DE MATÉRIEL</h1>
                 <p>
+                    // Afficher les données entrées dans les champs précédemment
                     Référence du matériel :<?php echo $_POST["ref_mat"]?><br>
                     Libéllé du matériel :<?php echo $_POST["lib_mat"]?><br>
                 </p>
+                // Bouton "Valider" 
                 <input type="submit" value="Valider ?"/>
             </form>    
         </center>
