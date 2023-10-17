7bcbca9331a4035ab9a992988f4f8137c9a8c389
fahime-dari <128680020+fahime-dari@users.noreply.github.com>
2023-10-17 16:00:38 +0200

    Add files via upload

--- resultat_log.php
+++ resultat_log.php
@@ -1,21 +1,16 @@
 <?php
-    session_start();
-    
-    
 
-    
-    
     if (isset($_POST['username']) && (isset($_POST['password'])))
     {
         $_SESSION['username']=$_POST['username'];
         $_SESSION['password']=$_POST['password'];
         $db=new PDO('mysql:host=localhost;dbname=rappel_tout',$_SESSION['username'],$_SESSION['password']);
        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       echo "Bien.";
+       header('Location: http://localhost/code/index.php');
     }
     else
     {
-        echo "Erreur";
+        echo "Erreur :";
     }
     
 
